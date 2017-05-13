9b8456924853dd7c9e4a5fb05a95834e8f655d2c
gabriel <gabriel@LightSaber>
2017-05-13 17:45:26 -0300

    v0003

--- minhaAssistencia/src/minhaAssistencia/JanelaPrincipal.java
+++ minhaAssistencia/src/minhaAssistencia/JanelaPrincipal.java
@@ -1,15 +1,23 @@
 package minhaAssistencia;
 
+import java.awt.Color;
+
 import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JPanel;
 
 public class JanelaPrincipal extends JFrame {
+	private JMenuBar menuBar;
+	private JMenu menuArquivo;
 	
 	public JanelaPrincipal() {
 		this.setTitle("minhaAssistência");
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.setSize(800, 600);
 		this.setLocationRelativeTo(null);
-		this.setVisible(true);
+		this.setVisible(true);	
+		
 	}
 	
 
---
+++ minhaAssistencia/src/minhaAssistencia/PainelPrincipal.java
@@ -0,0 +1,16 @@
+package minhaAssistencia;
+
+import javax.swing.JMenuBar;
+import javax.swing.JPanel;
+
+public class PainelPrincipal extends JPanel {
+	private JMenuBar menuBar;
+	
+	public PainelPrincipal() {
+		this.setLayout(null);
+		this.setSize(800, 600);
+		menuBar = new JMenuBar();
+		
+		
+	}
+}
